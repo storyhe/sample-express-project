857cddedb9d28783d2fd13eac1c2e0d0ee2fb178
Hyeonwoo Park <storyhe.love@gmail.com>
2020-09-28 12:42:19 +0900

    [FIX] ISSUE#1

--- app.js
+++ app.js
@@ -2,10 +2,23 @@ const express = require('express')
 const app = express()
 const port = 3000
 const sequelize = require('./src/db/models').sequelize
-
+const bodyParser = require('body-parser'); 
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const helmet = require('helmet'); 
 if (process.env.NODE_ENV === 'development') {
   sequelize.sync()
 }
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(cors({
+  origin: [],
+  credentials: true,
+}));
+
+app.use(helmet());
 
 app.use('/static', express.static(__dirname + '/src/static'))
 
